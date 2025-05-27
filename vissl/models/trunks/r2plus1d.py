0a44c779c0f27b612566d71689856eccf679e2a4
Paula Bigalke <paula.bigalke@uni-koeln.de>
2025-05-27 14:22:24 +0000

    create file for new model and paste code here - needs to be adapted now

---
+++ vissl/models/trunks/r2plus1d.py
@@ -0,0 +1,481 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+###### code adapted from https://github.com/kenshohara/3d-resnets-pytorch/blob/master/models/resnet2p1d.py/
+
+import logging
+from enum import Enum
+from typing import List
+
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torchvision.models.resnet import Bottleneck
+from vissl.config import AttrDict
+from vissl.data.collators.collator_helper import MultiDimensionalTensor
+from vissl.models.model_helpers import (
+    Flatten,
+    _get_norm,
+    get_trunk_forward_outputs,
+    get_tunk_forward_interpolated_outputs,
+    transform_model_input_data_type,
+)
+from vissl.models.trunks import register_model_trunk
+
+
+# For more depths, add the block config here
+BLOCK_CONFIG = {
+    50: (3, 4, 6, 3),
+    101: (3, 4, 23, 3),
+    152: (3, 8, 36, 3),
+    200: (3, 24, 36, 3),
+}
+
+
+class SUPPORTED_DEPTHS(int, Enum):
+    RN50 = 50
+    RN101 = 101
+    RN152 = 152
+    RN200 = 200
+
+
+class INPUT_CHANNEL(int, Enum):
+    lab = 1
+    bgr = 3
+    rgb = 3
+
+
+class SUPPORTED_L4_STRIDE(int, Enum):
+    one = 1
+    two = 2
+
+import math
+from functools import partial
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def get_inplanes():
+    return [64, 128, 256, 512]
+
+
+def conv1x3x3(in_planes, mid_planes, stride=1):
+    return nn.Conv3d(in_planes,
+                     mid_planes,
+                     kernel_size=(1, 3, 3),
+                     stride=(1, stride, stride),
+                     padding=(0, 1, 1),
+                     bias=False)
+
+
+def conv3x1x1(mid_planes, planes, stride=1):
+    return nn.Conv3d(mid_planes,
+                     planes,
+                     kernel_size=(3, 1, 1),
+                     stride=(stride, 1, 1),
+                     padding=(1, 0, 0),
+                     bias=False)
+
+
+def conv1x1x1(in_planes, out_planes, stride=1):
+    return nn.Conv3d(in_planes,
+                     out_planes,
+                     kernel_size=1,
+                     stride=stride,
+                     bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, downsample=None):
+        super().__init__()
+
+        n_3d_parameters1 = in_planes * planes * 3 * 3 * 3
+        n_2p1d_parameters1 = in_planes * 3 * 3 + 3 * planes
+        mid_planes1 = n_3d_parameters1 // n_2p1d_parameters1
+        self.conv1_s = conv1x3x3(in_planes, mid_planes1, stride)
+        self.bn1_s = nn.BatchNorm3d(mid_planes1)
+        self.conv1_t = conv3x1x1(mid_planes1, planes, stride)
+        self.bn1_t = nn.BatchNorm3d(planes)
+
+        n_3d_parameters2 = planes * planes * 3 * 3 * 3
+        n_2p1d_parameters2 = planes * 3 * 3 + 3 * planes
+        mid_planes2 = n_3d_parameters2 // n_2p1d_parameters2
+        self.conv2_s = conv1x3x3(planes, mid_planes2)
+        self.bn2_s = nn.BatchNorm3d(mid_planes2)
+        self.conv2_t = conv3x1x1(mid_planes2, planes)
+        self.bn2_t = nn.BatchNorm3d(planes)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1_s(x)
+        out = self.bn1_s(out)
+        out = self.relu(out)
+        out = self.conv1_t(out)
+        out = self.bn1_t(out)
+        out = self.relu(out)
+
+        out = self.conv2_s(out)
+        out = self.bn2_s(out)
+        out = self.relu(out)
+        out = self.conv2_t(out)
+        out = self.bn2_t(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1, downsample=None):
+        super().__init__()
+
+        self.conv1 = conv1x1x1(in_planes, planes)
+        self.bn1 = nn.BatchNorm3d(planes)
+
+        n_3d_parameters = planes * planes * 3 * 3 * 3
+        n_2p1d_parameters = planes * 3 * 3 + 3 * planes
+        mid_planes = n_3d_parameters // n_2p1d_parameters
+        self.conv2_s = conv1x3x3(planes, mid_planes, stride)
+        self.bn2_s = nn.BatchNorm3d(mid_planes)
+        self.conv2_t = conv3x1x1(mid_planes, planes, stride)
+        self.bn2_t = nn.BatchNorm3d(planes)
+
+        self.conv3 = conv1x1x1(planes, planes * self.expansion)
+        self.bn3 = nn.BatchNorm3d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2_s(out)
+        out = self.bn2_s(out)
+        out = self.relu(out)
+        out = self.conv2_t(out)
+        out = self.bn2_t(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self,
+                 block,
+                 layers,
+                 block_inplanes,
+                 n_input_channels=3,
+                 conv1_t_size=7,
+                 conv1_t_stride=1,
+                 no_max_pool=False,
+                 shortcut_type='B',
+                 widen_factor=1.0,
+                 n_classes=400):
+        super().__init__()
+
+        block_inplanes = [int(x * widen_factor) for x in block_inplanes]
+
+        self.in_planes = block_inplanes[0]
+        self.no_max_pool = no_max_pool
+
+        n_3d_parameters = 3 * self.in_planes * conv1_t_size * 7 * 7
+        n_2p1d_parameters = 3 * 7 * 7 + conv1_t_size * self.in_planes
+        mid_planes = n_3d_parameters // n_2p1d_parameters
+        self.conv1_s = nn.Conv3d(n_input_channels,
+                                 mid_planes,
+                                 kernel_size=(1, 7, 7),
+                                 stride=(1, 2, 2),
+                                 padding=(0, 3, 3),
+                                 bias=False)
+        self.bn1_s = nn.BatchNorm3d(mid_planes)
+        self.conv1_t = nn.Conv3d(mid_planes,
+                                 self.in_planes,
+                                 kernel_size=(conv1_t_size, 1, 1),
+                                 stride=(conv1_t_stride, 1, 1),
+                                 padding=(conv1_t_size // 2, 0, 0),
+                                 bias=False)
+        self.bn1_t = nn.BatchNorm3d(self.in_planes)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.maxpool = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, block_inplanes[0], layers[0],
+                                       shortcut_type)
+        self.layer2 = self._make_layer(block,
+                                       block_inplanes[1],
+                                       layers[1],
+                                       shortcut_type,
+                                       stride=2)
+        self.layer3 = self._make_layer(block,
+                                       block_inplanes[2],
+                                       layers[2],
+                                       shortcut_type,
+                                       stride=2)
+        self.layer4 = self._make_layer(block,
+                                       block_inplanes[3],
+                                       layers[3],
+                                       shortcut_type,
+                                       stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool3d((1, 1, 1))
+        self.fc = nn.Linear(block_inplanes[3] * block.expansion, n_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                nn.init.kaiming_normal_(m.weight,
+                                        mode='fan_out',
+                                        nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm3d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _downsample_basic_block(self, x, planes, stride):
+        out = F.avg_pool3d(x, kernel_size=1, stride=stride)
+        zero_pads = torch.zeros(out.size(0), planes - out.size(1), out.size(2),
+                                out.size(3), out.size(4))
+        if isinstance(out.data, torch.cuda.FloatTensor):
+            zero_pads = zero_pads.cuda()
+
+        out = torch.cat([out.data, zero_pads], dim=1)
+
+        return out
+
+    def _make_layer(self, block, planes, blocks, shortcut_type, stride=1):
+        downsample = None
+        if stride != 1 or self.in_planes != planes * block.expansion:
+            if shortcut_type == 'A':
+                downsample = partial(self._downsample_basic_block,
+                                     planes=planes * block.expansion,
+                                     stride=stride)
+            else:
+                downsample = nn.Sequential(
+                    conv1x1x1(self.in_planes, planes * block.expansion, stride),
+                    nn.BatchNorm3d(planes * block.expansion))
+
+        layers = []
+        layers.append(
+            block(in_planes=self.in_planes,
+                  planes=planes,
+                  stride=stride,
+                  downsample=downsample))
+        self.in_planes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.in_planes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1_s(x)
+        x = self.bn1_s(x)
+        x = self.relu(x)
+        x = self.conv1_t(x)
+        x = self.bn1_t(x)
+        x = self.relu(x)
+
+        if not self.no_max_pool:
+            x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def generate_model(model_depth, **kwargs):
+    assert model_depth in [10, 18, 34, 50, 101, 152, 200]
+
+    if model_depth == 10:
+        model = ResNet(BasicBlock, [1, 1, 1, 1], get_inplanes(), **kwargs)
+    elif model_depth == 18:
+        model = ResNet(BasicBlock, [2, 2, 2, 2], get_inplanes(), **kwargs)
+    elif model_depth == 34:
+        model = ResNet(BasicBlock, [3, 4, 6, 3], get_inplanes(), **kwargs)
+    elif model_depth == 50:
+        model = ResNet(Bottleneck, [3, 4, 6, 3], get_inplanes(), **kwargs)
+    elif model_depth == 101:
+        model = ResNet(Bottleneck, [3, 4, 23, 3], get_inplanes(), **kwargs)
+    elif model_depth == 152:
+        model = ResNet(Bottleneck, [3, 8, 36, 3], get_inplanes(), **kwargs)
+    elif model_depth == 200:
+        model = ResNet(Bottleneck, [3, 24, 36, 3], get_inplanes(), **kwargs)
+
+    return model
+
+
+@register_model_trunk("resnet")
+class ResNeXt(nn.Module):
+    """
+    Wrapper for TorchVison ResNet Model to support different depth and
+    width_multiplier. We provide flexibility with LAB input, stride in last
+    ResNet block and type of norm (BatchNorm, LayerNorm)
+    """
+
+    def __init__(self, model_config: AttrDict, model_name: str):
+        super(ResNeXt, self).__init__()
+        self.model_config = model_config
+        logging.info(
+            "ResNeXT trunk, supports activation checkpointing. {}".format(
+                "Activated"
+                if self.model_config.ACTIVATION_CHECKPOINTING.USE_ACTIVATION_CHECKPOINTING
+                else "Deactivated"
+            )
+        )
+
+        self.trunk_config = self.model_config.TRUNK.RESNETS
+        self.depth = SUPPORTED_DEPTHS(self.trunk_config.DEPTH)
+        self.width_multiplier = self.trunk_config.WIDTH_MULTIPLIER
+        self._norm_layer = _get_norm(self.trunk_config)
+        self.groups = self.trunk_config.GROUPS
+        self.zero_init_residual = self.trunk_config.ZERO_INIT_RESIDUAL
+        self.width_per_group = self.trunk_config.WIDTH_PER_GROUP
+        self.use_checkpointing = (
+            self.model_config.ACTIVATION_CHECKPOINTING.USE_ACTIVATION_CHECKPOINTING
+        )
+        self.num_checkpointing_splits = (
+            self.model_config.ACTIVATION_CHECKPOINTING.NUM_ACTIVATION_CHECKPOINTING_SPLITS
+        )
+
+        (n1, n2, n3, n4) = BLOCK_CONFIG[self.depth]
+        logging.info(
+            f"Building model: ResNeXt"
+            f"{self.depth}-{self.groups}x{self.width_per_group}d-"
+            f"w{self.width_multiplier}-{self._norm_layer.__name__}"
+        )
+
+        model = models.resnet.ResNet(
+            block=Bottleneck,
+            layers=(n1, n2, n3, n4),
+            zero_init_residual=self.zero_init_residual,
+            groups=self.groups,
+            width_per_group=self.width_per_group,
+            norm_layer=self._norm_layer,
+        )
+
+        model.inplanes = 64 * self.width_multiplier
+        dim_inner = 64 * self.width_multiplier
+        # some tasks like Colorization https://arxiv.org/abs/1603.08511 take input
+        # as L channel of an LAB image. In that case we change the input channel
+        # and re-construct the conv1
+        self.input_channels = INPUT_CHANNEL[self.model_config.INPUT_TYPE]
+
+        model_conv1 = nn.Conv2d(
+            self.input_channels,
+            model.inplanes,
+            kernel_size=7,
+            stride=2,
+            padding=3,
+            bias=False,
+        )
+        model_bn1 = self._norm_layer(model.inplanes)
+        model_relu1 = model.relu
+        model_maxpool = model.maxpool
+        model_avgpool = model.avgpool
+        model_layer1 = model._make_layer(Bottleneck, dim_inner, n1)
+        model_layer2 = model._make_layer(Bottleneck, dim_inner * 2, n2, stride=2)
+        model_layer3 = model._make_layer(Bottleneck, dim_inner * 4, n3, stride=2)
+
+        # For some models like Colorization https://arxiv.org/abs/1603.08511,
+        # due to the higher spatial resolution desired for pixel wise task, we
+        # support using a different stride. Currently, we know stride=1 and stride=2
+        # behavior so support only those.
+        safe_stride = SUPPORTED_L4_STRIDE(self.trunk_config.LAYER4_STRIDE)
+        model_layer4 = model._make_layer(
+            Bottleneck, dim_inner * 8, n4, stride=safe_stride
+        )
+
+        # we mapped the layers of resnet model into feature blocks to facilitate
+        # feature extraction at various layers of the model. The layers for which
+        # to extract features is controlled by requested_feat_keys argument in the
+        # forward() call.
+        self._feature_blocks = nn.ModuleDict(
+            [
+                ("conv1", model_conv1),
+                ("bn1", model_bn1),
+                ("conv1_relu", model_relu1),
+                ("maxpool", model_maxpool),
+                ("layer1", model_layer1),
+                ("layer2", model_layer2),
+                ("layer3", model_layer3),
+                ("layer4", model_layer4),
+                ("avgpool", model_avgpool),
+                ("flatten", Flatten(1)),
+            ]
+        )
+
+        # give a name mapping to the layers so we can use a common terminology
+        # across models for feature evaluation purposes.
+        self.feat_eval_mapping = {
+            "conv1": "conv1_relu",
+            "res1": "maxpool",
+            "res2": "layer1",
+            "res3": "layer2",
+            "res4": "layer3",
+            "res5": "layer4",
+            "res5avg": "avgpool",
+            "flatten": "flatten",
+        }
+
+    def forward(
+        self, x: torch.Tensor, out_feat_keys: List[str] = None
+    ) -> List[torch.Tensor]:
+        if isinstance(x, MultiDimensionalTensor):
+            out = get_tunk_forward_interpolated_outputs(
+                input_type=self.model_config.INPUT_TYPE,
+                interpolate_out_feat_key_name="res5",
+                remove_padding_before_feat_key_name="avgpool",
+                feat=x,
+                feature_blocks=self._feature_blocks,
+                feature_mapping=self.feat_eval_mapping,
+                use_checkpointing=self.use_checkpointing,
+                checkpointing_splits=self.num_checkpointing_splits,
+            )
+        else:
+            model_input = transform_model_input_data_type(
+                x, self.model_config.INPUT_TYPE
+            )
+            out = get_trunk_forward_outputs(
+                feat=model_input,
+                out_feat_keys=out_feat_keys,
+                feature_blocks=self._feature_blocks,
+                feature_mapping=self.feat_eval_mapping,
+                use_checkpointing=self.use_checkpointing,
+                checkpointing_splits=self.num_checkpointing_splits,
+            )
+        return out
